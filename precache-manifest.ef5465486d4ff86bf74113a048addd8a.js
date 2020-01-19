3732f456bd25ddedac33023936129053948a4611
k- <mahat.mejor@gmail.com>
2020-01-19 12:26:48 -0600

    Updates

---
+++ precache-manifest.ef5465486d4ff86bf74113a048addd8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014d207bf826ce2fea27061fec587ab2",
+    "url": "/kNews-react/index.html"
+  },
+  {
+    "revision": "2ff22d39ae62e2dce7f8",
+    "url": "/kNews-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "81e68f1f4a8bd265bb51",
+    "url": "/kNews-react/static/css/main.de7ce26e.chunk.css"
+  },
+  {
+    "revision": "2ff22d39ae62e2dce7f8",
+    "url": "/kNews-react/static/js/2.29be7b45.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/kNews-react/static/js/2.29be7b45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81e68f1f4a8bd265bb51",
+    "url": "/kNews-react/static/js/main.d2ed5841.chunk.js"
+  },
+  {
+    "revision": "64eff6954a4ff972b1be",
+    "url": "/kNews-react/static/js/runtime-main.03fca22e.js"
+  }
+]);
